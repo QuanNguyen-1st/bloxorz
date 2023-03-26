e36ebaf52ed4daca7343f83e81a0ec1345369a2d
Quan Nguyen <119392873+QuanNguyen-1st@users.noreply.github.com>
2023-03-26 18:41:13 +0700

    + add result for bfs and astar

---
+++ tester.py
@@ -0,0 +1,38 @@
+import os
+import pandas as pd
+import subprocess
+import time
+
+algorithm = "astar"
+levels = []
+time = []
+memory = []
+steps = []
+virtual_steps = []
+for i in range (1, 34):
+    level = str(i)
+    if len(level) < 2:
+        level = "0" + level
+    # cmd = string + " " + level + " " + algorithm
+    x = subprocess.check_output(['python', 'testrun.py', level, algorithm])
+    holder ,time_run, memory_run, steps_run, virtual_steps_run = x.decode().split(' ')
+    # print(float(time_run))
+    # print(float(memory_run))
+    levels.append(level)
+    time.append(float(time_run))
+    memory.append(float(memory_run))
+    steps.append(int(steps_run))
+    virtual_steps.append(int(virtual_steps_run))
+
+print(virtual_steps)
+
+data = {
+    'level':levels,
+    'time':time,
+    'memory':memory,
+    'steps':steps,
+    'virtual_steps':virtual_steps
+}
+df = pd.DataFrame(data)
+path = "result/result_" + algorithm + ".xlsx"
+df.to_excel(path, index = False)
---
+++ testrun.py
@@ -0,0 +1,34 @@
+import sys
+from atr.algorithm import Algorithm
+from testnogame import Game
+import os, psutil
+
+def main(level, algorithm):
+    game = Game(level)
+    if algorithm == Algorithm.NONE:
+        pass
+    else:
+        if algorithm == Algorithm.BFS:
+            game.solve_with_algorithm(Algorithm.BFS)
+        elif algorithm == Algorithm.ASTAR:
+            game.solve_with_algorithm(Algorithm.ASTAR)
+        elif algorithm == Algorithm.MCTS:
+            game.solve_with_algorithm(Algorithm.MCTS)
+
+
+if __name__== "__main__":
+    if len(sys.argv) > 2:
+        level = sys.argv[1]
+        algorithm = sys.argv[2]
+        if algorithm == "play": 
+            main(level=level, algorithm = Algorithm.NONE)
+        elif algorithm == "bfs":
+            main(level=level, algorithm = Algorithm.BFS)
+        elif algorithm == "astar":
+            main(level=level, algorithm = Algorithm.ASTAR)
+        elif algorithm == "mcts":
+            main(level=level, algorithm = Algorithm.MCTS)
+        else: 
+            print("Error! Read README.md for more details!")
+    else:
+        main(level="01", algorithm=Algorithm.BFS)
